36b68ba794ba9492cf0417d0492760e67c1ca0a9
Fatma072 <ftmx.na@gmail.com>
2025-07-13 10:59:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-Nama,Simbol,Nomor Atom,Massa Atom,Golongan,Jenis
-Hidrogen,H,1,1.008,1,Non-logam
-Helium,He,2,4.0026,18,Gas Mulia
-Litium,Li,3,6.94,1,Logam Alkali
-Berilium,Be,4,9.0122,2,Logam Alkali Tanah
-Bor,B,5,10.81,13,Metaloid
 
 import streamlit as st
 import pandas as pd
